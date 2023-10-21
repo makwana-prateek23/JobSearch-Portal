8feaa4bcf3d429e10898522d4b7bcb815c701311
makwana-prateek23 <makwanaprateek3@gmail.com>
2023-10-22 03:06:23 +0530

    Started the dashboard

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import "./App.css";
 import Navbar from "./NavBar";
 
 function App() {
-  const user = {
-    name: "Shivraj",
-    profilePicture: "https://example.com/profile.jpg",
-  };
-  return <Navbar user={user} />;
+  // const user = {
+  //   name: "Shivraj",
+  //   profilePicture: "https://example.com/profile.jpg",
+  // };
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
