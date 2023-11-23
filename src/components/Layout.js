920997073d2ee807b05bc8a1831764e6d7874f81
makwana-prateek23 <makwanaprateek3@gmail.com>
2023-11-23 19:57:44 +0530

    Corrected the Layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "./NavBar";
+import Footer from "./footer";
 
 function Layout({ children }) {
   return (
     <>
       <NavBar />
       {children}
+      <Footer />
     </>
   );
 }
