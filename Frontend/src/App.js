a0c31469a8faff52e67ac4e4f2b81f01a5e4ce64
makwana-prateek23 <makwanaprateek3@gmail.com>
2024-01-01 10:28:59 +0530

    updates

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,5 @@
-import React, { useState } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import AboutUs from "./Pages/AboutUs";
 import InternShips from "./Pages/InternShips";
@@ -12,69 +7,20 @@ import Jobs from "./Pages/Jobs";
 import Login from "./Pages/Login";
 import Main from "./Pages/Main";
 import Register from "./Pages/Register";
-import Dashboard from "./Pages/Dashboard";
-import Layout from "./components/Layout";
 
-const App = () => {
-  const [isAuthenticated, setAuthenticated] = useState(false);
-  const [userType, setUserType] = useState(null);
-
-  const handleLogin = (user, type) => {
-    // Your authentication logic here
-    setAuthenticated(true);
-    setUserType(type);
-    navigate("/dashboard");
-  };
-
+function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route
-          path="/about-us"
-          element={
-            <Layout>
-              {isAuthenticated ? <AboutUs /> : <Login onLogin={handleLogin} />}
-            </Layout>
-          }
-        />
-        <Route
-          path="/jobs"
-          element={
-            <Layout>
-              {isAuthenticated ? <Jobs /> : <Login onLogin={handleLogin} />}
-            </Layout>
-          }
-        />
-        <Route
-          path="/internships"
-          element={
-            <Layout>
-              {isAuthenticated ? (
-                <InternShips />
-              ) : (
-                <Login onLogin={handleLogin} />
-              )}
-            </Layout>
-          }
-        />
-        <Route path="/login" element={<Login onLogin={handleLogin} />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/jobs" element={<Jobs />} />
+        <Route path="/internships" element={<InternShips />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route
-          path="/dashboard"
-          element={
-            isAuthenticated ? (
-              <Layout>
-                <Dashboard userType={userType} />
-              </Layout>
-            ) : (
-              <Navigate to="/login" />
-            )
-          }
-        />
       </Routes>
     </Router>
   );
-};
+}
 
 export default App;
